5ca77c69a48aca865fccbd4641da795a98c8b88a
Alicius Schröder <37403182+aliciusschroeder@users.noreply.github.com>
2026-06-15 22:52:58 +0200

    chore: replace demos with 1 single one

---
+++ demo.bat
@@ -0,0 +1,49 @@
+cd dist
+
+sam -debug to bee or not to bee, that is the question
+sam -phonetic "MAY4 NEYM IHZ SAE4M."
+sam -pitch 64 -speed 72 -phonetic "FOHR YOHR KAA4MIXDOH6R SIH4KSTIYFOH6R KUMPYUW4TER."
+sam -mouth 110 -throat 160 -pitch 64 -speed 72 -phonetic "AY4 KAEN CHEY4NJ MAY VOY5S KUMPLIY3TLIY.Q."
+
+sam I will sing the national anthem of the united states of America.
+sam The star spangled banner!
+sam -sing -speed 40 -pitch 64 -phonetic ohohoh
+sam -sing -speed 40 -pitch 76 -phonetic ohohoh
+sam -sing -speed 40 -pitch 96 -phonetic sehehehehehehehehehey
+sam -sing -speed 40 -pitch 76 -phonetic kaeaeaeaeaeaeaeaeaen
+sam -sing -speed 40 -pitch 64 -phonetic yuxuxuxuxuxuxw
+sam -sing -speed 40 -pitch 48 -phonetic siyiyiyiyiyiyiyiyiyiyiyiyiyiyiyiyiyiy
+sam -sing -speed 40 -pitch 38 -phonetic baaaaay
+sam -sing -speed 40 -pitch 42 -phonetic dhaaaxaxaxax
+sam -sing -speed 40 -pitch 48 -phonetic daoaoaoaoaoaoaonz
+sam -sing -speed 40 -pitch 76 -phonetic ererererererer
+sam -sing -speed 40 -pitch 68 -phonetic liyiyiyiyiyiyiyiyiy
+sam -sing -speed 40 -pitch 64 -phonetic laaaaaaaaaaaaaaaaaaaaaaaaayt
+sam -sing -speed 40 -pitch 64 -phonetic whahahaht
+sam -sing -speed 40 -pitch 64 -phonetic sohohuw
+sam -sing -speed 40 -pitch 38 -phonetic praaaaaaaaaaaaaaaauwd
+sam -sing -speed 40 -pitch 42 -phonetic liyiyiy
+
+sam Now I sing Jazz
+sam -sing -pitch 96 -speed 50 It's Jazzy Jazzy Jazzy 
+sam -sing -pitch 68 -speed 50 Jazzy Jazzy Jazzy 
+sam -sing -pitch 48 -speed 50 Jazzy Jazzy Jazzy 
+sam -sing -pitch 32 -speed 50 Jazzy Jazzy Jazzy 
+sam -sing -pitch 96 -speed 50 Jazzy
+
+sam Roleplay
+sam -mouth 145 -throat 135 -pitch 40 -speed 72 -phonetic "AY5 KAEN TAO5K LAYK AH LIH5TUL OH5LD LEY5DIY.- SWIY5TIY."
+
+sam -mouth 145 -throat 125 -pitch 40 -speed 72 "You can do anything, but not everything."
+sam -mouth 135 -throat 135 -pitch 35 -speed 72 "The richest man is not he who has the most. But he who wants the least."
+sam -mouth 145 -throat 135 -pitch 40 -speed 72 "To the man who only has a hammer, everything he encounters looks like a nail."
+sam -mouth 145 -throat 125 -pitch 35 -speed 72 "Before I got married I had six ideas about bringing up kids; now I have six kids. And no idea?"
+sam -mouth 135 -throat 135 -pitch 40 -speed 72 "Always forgive your enemies. Nothing annoys them more."
+sam -mouth 145 -throat 125 -pitch 35 -speed 72 "All people are frauds. The only difference between them is that some admit it. I myself totally dee-nigh. it."
+sam -mouth 135 -throat 135 -pitch 40 -speed 72 "I do not mind what Con-gress does. As long as they do not do it. in the streets. And frighten the kids!"
+
+sam -speed 55 -pitch 92 -throat 128 -mouth 128 "Ha ha ha ha ha ha ha? Ha ha ha ha ha ha ha!"
+sam That's not funny grandma.
+
+sam -mouth 145 -throat 125 -pitch 40 -speed 72 "O.K. Let grandma take it from here."
+sam -mouth 145 -throat 135 -pitch 40 -speed 72 -phonetic "SWIY5TIY."
